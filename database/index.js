a4b597169c5c114383947e68e5b2d9c219e6501f
Theresa <tli2@cca.edu>
2019-03-23 11:04:47 -0700

    Implement deletion of same user repos to save function

--- database/index.js
+++ database/index.js
@@ -13,9 +13,8 @@ let repoSchema = mongoose.Schema({
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (results) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+  Repo.deleteMany({username: result.owner.login}, (err) => { throw err });
+
   results.map((result) => {
     var newRepo = new Repo({
       username: result.owner.login,
@@ -26,16 +25,10 @@ let save = (results) => {
       stargazers: result.stargazers_count
     });
 
-    console.log("a repo:", {
-      username: result.owner.login,
-      userLink: result.owner.url,
-      avatar: result.owner.avatar_url,
-      repoName: result.name,
-      repoLink: result.html_url,
-      stargazers: result.stargazers_count
+    newRepo.save((err, newRepo) => {
+      if (err) throw err;
     });
   });
-  
 }
 
 module.exports.save = save;
