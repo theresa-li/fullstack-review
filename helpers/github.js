60f494206d81da4a6b6d534a148def9a52bc553b
Theresa <tli2@cca.edu>
2019-03-22 20:29:11 -0700

    Connect to Github API

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,26 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (user) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
+  
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    url: `https://api.github.com/users/${user}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+
+  request(options, (error, response, body) => {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
 
 }
 
